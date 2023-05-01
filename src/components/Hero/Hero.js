74afe8f0b175ec0d5cbf7ef75055aae4c4275f9e
yaroshis <iaros1298@gmail.com>
2023-05-01 20:34:04 +0300

    Fix for optimized hero image (use avif)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,20 +5,20 @@ const Hero = () => {
   return (
     <Wrapper>
       <picture>
+       <source
+          type="image/avif"
+          srcSet="
+            /images/hero-img.avif 1x,
+            /images/hero-img@2x.avif 2x,
+            /images/hero-img@3x.avif 3x
+          "
+        />
         <source
           type="image/jpeg"
           srcSet="
             /images/hero-img.jpg 1x,
             /images/hero-img@2x.jpg 2x,
             /images/hero-img@3x.jpg 3x
-          "
-        />
-        <source
-          type="image/avif"
-          srcSet="
-            /images/hero-img.avif 1x,
-            /images/hero-img@2x.avif 2x,
-            /images/hero-img@3x.avif 3x
           "
         />
         <HeroImage src="/images/hero-img.jpg" />
